18adda63e96c24f1115ab69cdb4c9844d6feb5d7
Jakub Trhlík <trhlijak@fit.cvut.cz>
2018-08-15 00:42:19 +0200

    working prototype

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,11 +1,55 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+const keyboard = [
+  [],
+  [],
+  ["a", "b", "c"],
+  ["d", "e", "f"],
+  ["g", "h", "i"],
+  ["j", "k", "l"],
+  ["m", "n", "o"],
+  ["p", "q", "r", "s"],
+  ["t", "u", "v"],
+  ["w", "x", "y", "z"],
+];
+
+/* GET api results listing. */
 router.get('/', function(req, res, next) {
 
+  console.log(req.query.dialed);
 
+  let result = [];
 
+  if (req.query.dialed && Number.isInteger(parseInt(req.query.dialed))) {
+
+    let sDialed = req.query.dialed.toString();
+
+    for (let n = 0; n < keyboard[parseInt(sDialed[0], 10)].length; n++) {
+      result.push(keyboard[parseInt(sDialed[0], 10)][n]);
+    }
+
+    //for each char in number
+    for (var i = 1; i < sDialed.length; i++) {
+
+      let tmpres = [];
+      var keyint = parseInt(sDialed[i], 10);
+      //extend results
+      for (var p = 0; p < result.length; p++) {
+        //by the possibilities
+        for (var n = 0; n < keyboard[keyint].length; n++) {
+          tmpres.push(result[p] + keyboard[keyint][n]);
+        }
+      }
+      result = tmpres;
+
+    }
+
+    res.json(result);
+
+  } else {
+        res.status(404).send();
+  }
 });
 
 module.exports = router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 
-state= { users: [] }
+  constructor(props) {
+    super(props);
 
-  componentDidMount(){
-    fetch('/users')
-    .then(res => res.json())
-    .then(users => this.setState({users}))
+    this.state = { response: [] , text: "", word: []};
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
+  componentDidMount() {
+    fetch('/api')
+      .then(res => res.json())
+      .then(response => this.setState({ response }));
+  }
+
+  handleKeyPress(e){
+       let key=e.key;
+       let text =this.state.text;
+
+       if(Number.isInteger(parseInt(key,10))){
+       text = this.state.text + key;
+     }else if(key === "Backspace"){
+       text = text.substring(0, text.length-1);
+     }
+       this.setState({
+           text:text,
+       })
+
+       fetch('/api?dialed='+text)
+         .then(res => res.json())
+         .then(response => this.setState({ response }));
+
+   }
+
 
   render() {
     return (
       <div className="App">
-<h1>Users</h1>
-<ul>
-{this.state.users.map(user => <li key={user.id}> {user.user}</li>
 
-)}
-</ul>
+        <h1>T9 Emulator</h1>
+
+        <input type="text" value={this.state.text} name="fname" onKeyDown={this.handleKeyPress} /><br />
+
+<div>{this.state.text}</div>
+
+<p>
+            {
+              this.state.response.map((res,index) => <span key={index}> {res}, </span>)
+          }
+  </p>
       </div>
-    );
-  }
-}
+        );
+      }
+    }
 
-export default App;
+    export default App;
